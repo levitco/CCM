fbb20460da1ac675d41a8b852ebe4bb37a6fbe34
levitco <oldi2002@gmail.com>
2021-03-16 14:34:52 -0600

    03/16/21 14:34:52

--- gitpull.sh
+++ gitpull.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m "$(date +"%D %T")"
 pass="classicmalkvian321"
 /usr/bin/expect <<EOD
-spawn git pull
+spawn git push
 expect "Enter passphrase for key '/home/$USERNAME/.ssh/id_ed25519':"
 send "$pass\r"
 expect eof
